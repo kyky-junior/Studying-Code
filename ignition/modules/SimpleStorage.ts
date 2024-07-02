6ef0d8608c4a8611da9a7d2c037b73e6e5a8d7e4
kyky-junior <ngakojunior01@gmail.com>
2024-07-02 15:06:17 +0100

    Update SimpleStorage.ts

--- ignition/modules/SimpleStorage.ts
+++ ignition/modules/SimpleStorage.ts
@@ -1,7 +1,5 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI: bigint = 1_000_000_000n;
 
 const SimpleStorageModule = buildModule("SimpleStorageModule", (m) => {
  
